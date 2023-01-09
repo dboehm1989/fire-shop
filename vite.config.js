10bbba5fabd19c827093935048bef2733e238c0e
dboehm <dboehm_89@hotmail.de>
2023-01-09 21:06:55 +0100

    create new project & fixed code settings

---
+++ vite.config.js
@@ -0,0 +1,30 @@
+import { defineConfig } from 'vite';
+import vue from '@vitejs/plugin-vue';
+import { fileURLToPath, URL } from 'node:url';
+import AutoImport from 'unplugin-auto-import/vite';
+
+export default defineConfig(({ command, mode }) => {
+  return {
+    plugins: [
+      vue(),
+      AutoImport({
+        imports: ['vue'],
+        dts: 'src/auto-imports.d.ts',
+        vueTemplate: true,
+      }),
+    ],
+    server: {
+      port: 8080,
+    },
+    resolve: {
+      alias: {
+        '@': fileURLToPath(new URL('./src', import.meta.url)),
+        '~': fileURLToPath(new URL('./node_modules', import.meta.url)),
+      },
+    },
+    build: {
+      emptyOutDir: true,
+      chunkSizeWarningLimit: 1000,
+    },
+  };
+});
