32ec5d806c0f00c7c079dc09eed38976f01abbc9
dboehm <dboehm_89@hotmail.de>
2023-01-11 00:03:40 +0100

    add reativity to image

--- src/components/product/description/product-image/ProductImage.vue
+++ src/components/product/description/product-image/ProductImage.vue
@@ -1,13 +1,32 @@
 <template>
   <div class="image-block">
     <div class="preview-images">
-      <img class="preview-image active" src="@/assets/img/thumb_ultimate.png" alt="Ultimate Image 1" />
-      <img class="preview-image" src="@/assets/img/thumb_ultimate-01.png" alt="Ultimate Image 2" />
-      <img class="preview-image" src="@/assets/img/thumb_ultimate-02.png" alt="Ultimate Image 3" />
+      <img
+        v-for="img in imgs"
+        :key="img.id"
+        class="preview-image"
+        :class="{ active: isActive(img.id) }"
+        :alt="img.name"
+        :src="`/src/assets/img/${img.preview}`"
+        @click="selectedID = img.id"
+      />
     </div>
-    <div class="large-image active" style="background-image: url(src/assets/img/ultimate.png)" />
+    <div class="large-image active" :style="backgroundImageStyle" />
   </div>
 </template>
+
+<script setup lang="ts">
+const imgs = [
+  { id: 1, name: 'Ultimate Image 1', preview: 'thumb_ultimate.png', original: 'ultimate.png' },
+  { id: 2, name: 'Ultimate Image 2', preview: 'thumb_ultimate-01.png', original: 'ultimate-01.png' },
+  { id: 3, name: 'Ultimate Image 3', preview: 'thumb_ultimate-02.png', original: 'ultimate-02.png' },
+];
+
+const selectedID = ref(1);
+const isActive = (id: number) => selectedID.value === id;
+const selectedImg = computed(() => imgs.find(img => isActive(img.id)));
+const backgroundImageStyle = computed(() => `background-image: url(src/assets/img/${selectedImg.value?.original})`);
+</script>
 
 <style lang="scss" scoped>
 @import './product-image.scss';
